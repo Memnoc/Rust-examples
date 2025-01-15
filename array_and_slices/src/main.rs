f74504a7fe3724a7ba501c03ad59b459a5a5ac9d
Memnoc <smartdroidesign@gmail.com>
2025-01-15 13:14:26 +0000

    beginning of array and slices

---
+++ array_and_slices/src/main.rs
@@ -0,0 +1,19 @@
+use std::mem;
+
+fn analyze_slice(slice: &[i32]) {
+    println!("First element of the slice: {}", slice[0]);
+    println!("The slice has {} elements", slice.len());
+}
+
+fn main() {
+    println!("== Array and Slices ==");
+    // Fixed-size array (type signature is superfluous).
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // All elements can be initialised to the same value
+    let ys: [i32; 500] = [0; 500];
+
+    // Indexing starts at 0
+    println!("First element of the array: {}", xs[0]);
+    println!("Second element of the array: {}", xs[1]);
+}
