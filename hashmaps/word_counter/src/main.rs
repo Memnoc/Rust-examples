833910ed837ce17f4de02b1daa7d841be4aa6f23
Memnoc <smartdroidesign@gmail.com>
2025-03-01 15:48:27 +0000

    example for the frequency word counter

--- hashmaps/word_counter/src/main.rs
+++ hashmaps/word_counter/src/main.rs
@@ -1,7 +1,14 @@
 use std::collections::HashMap;
 
+// HEADER:
 fn word_frequency(text: &str) -> HashMap<String, usize> {
-    todo!()
+    let mut word_counts = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let word_lower = word.to_lowercase();
+        *word_counts.entry(word_lower).or_insert(0) += 1;
+    }
+    word_counts
 }
 
 #[cfg(test)]
@@ -17,4 +24,7 @@ mod test {
 
 fn main() {
     println!("=== Word Counter ===");
+    let test_word = "hello hello hello";
+    let result = word_frequency(&test_word);
+    println!("{:?}", result);
 }
